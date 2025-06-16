a67d9fa7f98685f036b18ce67871bbab487e4958
Prem324 <prem324r@gmail.com>
2025-06-16 22:34:44 +0530

    Portfolio

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Certificates() {
-  return <div>Certificates</div>;
+  return (
+    <div>
+      <h1>Certificates</h1>
+      {/* Add your certificates here */}
+      <ul>
+        <li>Certificate of Completion - Full Stack Developer</li>
+        <li>Certificate of Completion - JavaScript Developer</li>
+        <li>Certificate of Completion - React Developer</li>
+      </ul>
+      {/* Add your certificates here */}
+      {/* Add more certificates as needed */}
+    </div>
+  );
 }
 
 export default Certificates;
